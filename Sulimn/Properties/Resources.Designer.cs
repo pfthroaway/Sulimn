c7cbd4289f892773e41d815cf3e2e5145a142cb9
pfthroaway <pfthroaway@mail.com>
2016-11-07 23:06:45 -0600

    Alpha 1 - Update 2 - Refactor classes/windows/etc
    I'm reorganizing the project in preparation of switching to SQLite.

--- Sulimn/Properties/Resources.Designer.cs
+++ Sulimn/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Sulimn_WPF.Properties
-{
-
-
+namespace Sulimn.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -39,7 +39,7 @@ namespace Sulimn_WPF.Properties
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sulimn_WPF.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sulimn.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
