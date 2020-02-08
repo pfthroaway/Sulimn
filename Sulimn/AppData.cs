5d61595769b574698af482ca8f77a29941f22c01
pfthroaway <pfthroaway@mail.com>
2020-02-08 01:39:41 -0600

    Update AppData.cs

--- Sulimn/AppData.cs
+++ Sulimn/AppData.cs
@@ -6,6 +6,6 @@ namespace Sulimn
     public static class AppData
     {
         internal static string Location = Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Sulimn");
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PF Software", "Sulimn");
     }
 }
